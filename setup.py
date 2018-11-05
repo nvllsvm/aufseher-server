d7ed69bdc47f074f31d5dbf868110dd1d7ca4297
Andrew Rabert <ar@nullsum.net>
2018-11-05 18:08:29 -0500

    Setup tweaks

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 import setuptools
 
+
 setuptools.setup(
     name='aufseher',
-    version='0.1.0',
-    install_requires=['tornado>=5',
-                      'pycurl',
-                      'pyyaml'],
+    install_requires=[
+        'tornado>=5',
+        'pycurl',
+        'pyyaml'
+    ],
     py_modules=['aufseher'],
     entry_points={
         'console_scripts': ['aufseher = aufseher:main']
-    }
+    },
+    setuptools_requires=['setuptools_scm'],
+    use_setuptools_scm=True
 )
