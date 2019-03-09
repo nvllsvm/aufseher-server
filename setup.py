24d3724e590230c4d21742e0ea560b7d3163c4f3
Andrew Rabert <ar@nullsum.net>
2019-03-09 15:16:45 -0500

    Update setup

--- setup.py
+++ setup.py
@@ -3,15 +3,14 @@ import setuptools
 
 setuptools.setup(
     name='aufseher',
+    version='0.0.0',
     install_requires=[
-        'tornado>=5',
+        'tornado>=6',
         'pycurl',
         'pyyaml'
     ],
     py_modules=['aufseher'],
     entry_points={
         'console_scripts': ['aufseher = aufseher:main']
-    },
-    setup_requires=['setuptools_scm'],
-    use_scm_version=True
+    }
 )
